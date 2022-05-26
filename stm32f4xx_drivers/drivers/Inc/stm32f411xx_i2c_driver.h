ae042aec3b4c7276a5375a1a5c6d2d22e1cfb829
BachPham165 <bachphamduy165@gmail.com>
2022-05-26 10:17:56 +0700

    Update stm32f411xx_i2c_driver.h

--- stm32f4xx_drivers/drivers/Inc/stm32f411xx_i2c_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f411xx_i2c_driver.h
@@ -87,7 +87,7 @@ typedef struct{
 
 #define I2C_EV_TX_CMPLT		0
 #define I2C_EV_RX_CMPLT		1
-#define I2C_EV_STOP			2
+#define I2C_EV_STOP		2
 #define I2C_ERROR_BERR  	3
 #define I2C_ERROR_ARLO  	4
 #define I2C_ERROR_AF    	5
@@ -101,7 +101,7 @@ typedef struct{
  * I2C application states
  */
 
-#define I2C_READY					0
+#define I2C_READY				0
 #define I2C_BUSY_IN_RX				1
 #define I2C_BUSY_IN_TX				2
 
