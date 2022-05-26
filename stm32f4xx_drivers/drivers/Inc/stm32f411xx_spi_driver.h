7e232e1566161288f48d6779b2a68b4c4cdee5bf
BachPham165 <bachphamduy165@gmail.com>
2022-05-26 10:16:53 +0700

    Update stm32f411xx_spi_driver.h

--- stm32f4xx_drivers/drivers/Inc/stm32f411xx_spi_driver.h
+++ stm32f4xx_drivers/drivers/Inc/stm32f411xx_spi_driver.h
@@ -46,17 +46,17 @@ typedef struct{
  */
 
 #define SPI_READY			0
-#define SPI_BUSY_IN_RX		1
-#define SPI_BUSY_IN_TX		2
+#define SPI_BUSY_IN_RX			1
+#define SPI_BUSY_IN_TX			2
 
 /*
  * Posible SPI Application events
  */
 
-#define SPI_EVENT_TX_CMPLT	1
-#define SPI_EVENT_RX_CMPLT	2
-#define SPI_EVENT_OVR_ERR	3
-#define SPI_EVENT_CRC_ERR	4
+#define SPI_EVENT_TX_CMPLT		1
+#define SPI_EVENT_RX_CMPLT		2
+#define SPI_EVENT_OVR_ERR		3
+#define SPI_EVENT_CRC_ERR		4
 
 
 
@@ -65,23 +65,23 @@ typedef struct{
  * SPI_DeviceMode
  */
 
-#define SPI_DEVICE_MODE_MASTER 1
-#define SPI_DEVICE_MODE_SLAVE  0
+#define SPI_DEVICE_MODE_MASTER 		1
+#define SPI_DEVICE_MODE_SLAVE  		0
 
 /*
  * BusConfig
  */
-#define SPI_BUS_CONFIG_FD 			1
-#define SPI_BUS_CONFIG_HD			2
+#define SPI_BUS_CONFIG_FD 		1
+#define SPI_BUS_CONFIG_HD		2
 #define SPI_BUS_CONFIG_S_RXONLY		3
 
 /*
  * SPI CLock speed
  */
 
-#define SPI_SCLK_SPEED_DIV2			0
-#define SPI_SCLK_SPEED_DIV4			1
-#define SPI_SCLK_SPEED_DIV8			2
+#define SPI_SCLK_SPEED_DIV2		0
+#define SPI_SCLK_SPEED_DIV4		1
+#define SPI_SCLK_SPEED_DIV8		2
 #define SPI_SCLK_SPEED_DIV16		3
 #define SPI_SCLK_SPEED_DIV32		4
 #define SPI_SCLK_SPEED_DIV64		5
@@ -92,30 +92,30 @@ typedef struct{
  *SPI DFF
  */
 
-#define SPI_DFF_8BITS				0
-#define SPI_DFF_4BITS				1
+#define SPI_DFF_8BITS			0
+#define SPI_DFF_4BITS			1
 
 /*
  * CPOL
  */
 
-#define SPI_CPOL_HIGH 	1
-#define SPI_CPOL_LOW	0
+#define SPI_CPOL_HIGH 			1
+#define SPI_CPOL_LOW			0
 
 /*
  * CPHA
  */
 
-#define SPI_CPHA_HIGH	1
-#define SPI_CPHA_LOW	0
+#define SPI_CPHA_HIGH			1
+#define SPI_CPHA_LOW			0
 
 
 /*
  * SPI_SSM
  */
 
-#define SPI_SSM_EN		1
-#define SPI_SSM_DI		0
+#define SPI_SSM_EN			1
+#define SPI_SSM_DI			0
 
 
 #define SPI_TXE_FLAG	(1 << SPI_SR_TXE)
